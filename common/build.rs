e4acb8ffd46333aa7b952696d567531886375cfd
c-node <chuckhead7@gmail.com>
2025-01-29 12:37:44 -0500

    downgrade versions for zksync compatability :(

--- common/build.rs
+++ common/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Running build.rs");
     let code_gen_path = std::path::Path::new("src/generated");
     if !code_gen_path.exists() {
         std::fs::create_dir_all(code_gen_path)?;
@@ -7,6 +8,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .build_client(true)
         .out_dir(code_gen_path)
-        .compile_protos(&["proto/eqservice.proto"], &["proto/"])?;
+        //.compile_protos(&["proto/eqservice.proto"], &["proto/"])?;
+        .compile(&["proto/eqservice.proto"], &["proto/"])?;
     Ok(())
 }
